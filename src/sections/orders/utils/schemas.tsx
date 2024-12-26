c19de1b9d2c882c2b89c562602ab514b027df8ee
Adrian <adrian.suarez.alphen@gmail.com>
2024-12-26 13:44:17 -0500

    fix(orders): added error message for the status selection

--- src/sections/orders/utils/schemas.tsx
+++ src/sections/orders/utils/schemas.tsx
@@ -3,12 +3,19 @@ import z from "zod";
 
 export const updateOrderSchema = () =>
   z.object({
-    status: z.enum([
-      orderStatusMap.get(OrderStatus.PAYED) as string,
-      orderStatusMap.get(OrderStatus.PAYMENT_PENDING) as string,
-      orderStatusMap.get(OrderStatus.PROCESSING_PAYMENT) as string,
-      orderStatusMap.get(OrderStatus.REFUNDED) as string,
-      orderStatusMap.get(OrderStatus.DELIVERED) as string,
-      orderStatusMap.get(OrderStatus.CANCELLED) as string,
-    ]),
+    status: z.enum(
+      [
+        orderStatusMap.get(OrderStatus.PAYED) as string,
+        orderStatusMap.get(OrderStatus.PAYMENT_PENDING) as string,
+        orderStatusMap.get(OrderStatus.PROCESSING_PAYMENT) as string,
+        orderStatusMap.get(OrderStatus.REFUNDED) as string,
+        orderStatusMap.get(OrderStatus.DELIVERED) as string,
+        orderStatusMap.get(OrderStatus.CANCELLED) as string,
+      ],
+      {
+        errorMap: () => ({
+          message: "Se debe de seleccionar un estado para la orden.",
+        }),
+      }
+    ),
   });
