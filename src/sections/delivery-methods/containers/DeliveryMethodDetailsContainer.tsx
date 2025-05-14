93efac6711a57ab997a939a9eff1234e3918aaf5
AfroAg02 <afro.aguirre1969@gmail.com>
2025-05-14 09:37:13 -0400

    fix: delivery method container

--- src/sections/delivery-methods/containers/DeliveryMethodDetailsContainer.tsx
+++ src/sections/delivery-methods/containers/DeliveryMethodDetailsContainer.tsx
@@ -2,9 +2,13 @@
 import DeliveryMethodDetails from "../components/DeliveryMethodDetails";
 import { useContext } from "react";
 import { ModalContext } from "@/components/partials/Modal/context/ModalContext";
+import useDeliveryMethod from "../hooks/useDeliveryMethod";
 
 export default function DeliveryMethodDetailsContainer() {
   const { entityId } = useContext(ModalContext);
+    const deliveryMethodData = useDeliveryMethod({
+      deliveryMethodId: entityId ?? "",
+    });
 
-  return <DeliveryMethodDetails deliveryMethodId={entityId} />;
+  return <DeliveryMethodDetails deliveryMethodData={deliveryMethodData} />;
 }
